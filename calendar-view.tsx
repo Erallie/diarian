4cf9b47f17d806a18fcd8fa23a8e57d219be1810
Erallie <31375589+Erallie@users.noreply.github.com>
2024-08-07 15:43:37 -0700

    Working basis

--- calendar-view.tsx
+++ calendar-view.tsx
@@ -1,6 +1,13 @@
 import { StrictMode } from "react";
 import { ItemView, WorkspaceLeaf } from "obsidian";
 import { Root, createRoot } from "react-dom/client";
+import { Calendar } from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { useState } from 'react';
+type ValuePiece = Date | null;
+
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const CALENDAR_VIEW_TYPE = "calendar-view";
 
@@ -23,7 +30,7 @@ export class CalendarView extends ItemView {
         this.root = createRoot(this.containerEl.children[1]);
         this.root.render(
             <StrictMode>
-                <Calendar />,
+                <Container />
             </StrictMode>,
         );
     }
@@ -33,6 +40,24 @@ export class CalendarView extends ItemView {
     }
 }
 
-const Calendar = () => {
-    return <h4>Hello, React!</h4>;
+const Container = () => {
+    let notesToShow;
+    const today = new Date();
+    const [date, setDate] = useState(new Date());
+    // const dateString = 
+    if (date.getDay() == today.getDay() && date.getMonth() == today.getMonth() && date.getFullYear() == today.getFullYear()) {
+        notesToShow = <p>Insert notes to show here!</p>;
+    } else {
+        notesToShow = <p>There are no notes on this day.</p>;
+    }
+    return (
+        <div>
+            <Calendar onClickDay={setDate} value={date} />
+            {/* set "formatDay" to be changed in the settings */}
+            {date.toString()}
+            {notesToShow}
+            {/* Add functionality to display  */}
+            {/* <h4>Hello, React!</h4> */}
+        </div>
+    )
 };
