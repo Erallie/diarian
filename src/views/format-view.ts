a1d41e8189dda0cdeec73f3495d9a0a4708ff789
Erallie <31375589+Erallie@users.noreply.github.com>
2024-09-24 10:57:34 -0700

    Make format-view more understandable
    Added the description and adjusted the settings text and names so that people can more easily understand what they do.

--- src/views/format-view.ts
+++ src/views/format-view.ts
@@ -13,18 +13,30 @@ export class ConvertView extends Modal {
 
     onOpen() {
         const { contentEl } = this;
-        new Setting(contentEl).setName('Convert date formats').setHeading();
 
         const infoDesc = new DocumentFragment();
-        infoDesc.textContent = 'Begin the conversion process.'
+        infoDesc.textContent = 'This tool allows you to rename/move pre-existing daily notes to match the format specified under '
+        infoDesc.createEl('strong', { text: 'Settings → Daily notes → Date format' })
+        infoDesc.createEl('span', { text: ' and the folder specified under ' }).createEl('strong', { text: 'Settings → Daily notes → New file location' })
+        infoDesc.createEl('span', { text: '.' }).createEl('br');
+        // const infoList = infoDesc.createEl('ul')
         infoDesc.createEl('br')
-        infoDesc.createEl('span', { text: 'View ' }).createEl('a', {
+        // const infoBullet = infoList.createEl('li', { text: 'If you wish to change the format and/or folder of pre-existing daily notes, the above settings should be adjusted ' })
+        infoDesc.createEl('span', { text: 'If you wish to change the format and/or folder of pre-existing daily notes, the above settings should be adjusted ' }).createEl('em', { text: 'before' })
+        infoDesc.createEl('span', { text: ' using this tool.' });
+
+        new Setting(contentEl).setName('Convert date formats').setHeading().setDesc(infoDesc);
+
+        /* const startDesc = new DocumentFragment();
+        startDesc.textContent = 'Begin the conversion process.'
+        startDesc.createEl('br')
+        startDesc.createEl('span', { text: 'View ' }).createEl('a', {
             text: 'converter notes',
             attr: {
                 href: 'https://github.com/Erallie/diarian?tab=readme-ov-file#date-format-converter'
             }
         })
-        infoDesc.createEl('span', { text: '.' });
+        startDesc.createEl('span', { text: '.' }); */
         /* infoDesc.createEl('span', { text: 'All notes that match will be renamed to the format specified under ' }).createEl('strong', { text: 'Settings → Daily notes → Date format' });
         infoDesc.createEl('span', { text: '.' }).createEl('br');
         infoDesc.createEl('span', { text: 'Only notes found under the folder specified in ' }).createEl('strong', { text: 'Settings → Daily notes → New file location' });
@@ -39,11 +51,11 @@ export class ConvertView extends Modal {
 
 
         const formatPreview = new DocumentFragment();
-        formatPreview.textContent = 'A note with the path: ';
+        formatPreview.textContent = 'A daily note with the path: ';
         const folderSample = formatPreview.createEl('span', { text: oldFolder, cls: 'text-accent' });
         const fromSample = formatPreview.createSpan({ cls: 'text-accent' })
         formatPreview.createEl('br');
-        formatPreview.createEl('span', { text: 'Will be renamed to: ' })
+        formatPreview.createEl('span', { text: 'Will be renamed/moved to: ' })
             .createEl('span', { text: folder + moment().format(format), cls: 'text-accent' });
 
         // const toSample = formatPreview.createSpan({ cls: 'text-accent' });
@@ -51,8 +63,8 @@ export class ConvertView extends Modal {
         let oldFormat = format;
 
         new Setting(contentEl)
-            .setName('Format to convert from')
-            .setDesc('Enter the date format of the notes you\'d like to rename.')
+            .setName('Old date format')
+            .setDesc('Enter the old date format of the daily notes you\'d like to convert.')
             .addMomentFormat((from) => {
                 from
                     .setDefaultFormat(format)
@@ -63,14 +75,14 @@ export class ConvertView extends Modal {
                     })
             })
 
-        const folderDesc = new DocumentFragment();
+        /* const folderDesc = new DocumentFragment();
         folderDesc.textContent = 'Enter old folder path previously set under '
         folderDesc.createEl('strong', { text: 'Settings → Daily notes → New file location' })
-        folderDesc.createEl('span', { text: '.' });
+        folderDesc.createEl('span', { text: '.' }); */
 
         new Setting(contentEl)
             .setName('Old daily note folder')
-            .setDesc('Enter old folder path of the notes you\'d like to rename.')
+            .setDesc('Enter old folder path of the daily notes you\'d like to convert.')
             .addText((text) => {
                 text
                     .setValue(normalizePath(oldFolder))
@@ -92,7 +104,7 @@ export class ConvertView extends Modal {
 
         const convertSetting = new Setting(contentEl)
             .setName('Start conversion')
-            .setDesc(infoDesc)
+            .setDesc('Begin the conversion process.')
 
         const convertButton = convertSetting.controlEl.createEl("button");
         setIcon(convertButton, 'refresh-ccw');
